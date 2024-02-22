8f7b623592afd79696b8141217e4ebe7697f331a
juliguez <julirodriguezandrade@gmail.com>
2024-02-22 12:35:49 -0500

    NUEVO

--- Empleado/Empleado.py
+++ Empleado/Empleado.py
@@ -1,4 +1,44 @@
 class Empleado:
     # Aqui el copdigo}
     # Atributos
-    nombre = 0
+    Nombre = ""
+    Apellido = ""
+    Sexo = ""
+    # 1= masculino 2= femenino
+    Salario = 0
+    '''---------------------------------------------------
+    ------------------------------------------------------'''
+    # Metodos 
+    def CambiarSalario(self, nuevoSalario):
+    # Aqui va el codigo del metodo 
+        return 0
+
+    def CambiarEmpleado(self, nNombre, nApellido, nSexo, nSalario):
+    # Aqui va el codigo del nuevo Empleado
+        return None 
+        
+    def ConsultarSalario(self):
+    # Aqui va el codigo del metodo
+        return self.Salario
+    
+    def ConsultarNombre(self):
+    # Aqui va el codigo del metodo
+        return self.Nombre
+    
+    def ConsultarApellido(self):
+    # Aqui va el codigo del metodo
+        return self.Apellido
+    
+    def ConsultarNombrecompleto(self):
+    # Aqui va el codigo del metodo
+        return self.Nombre +" "+ self.Apellido
+    
+        Juliana Rodriguez
+    
+    def AumentoSalarial(self):
+        nSalario= self.Salario =
+    
+    
+    
+    
+   
