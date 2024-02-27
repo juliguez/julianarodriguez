796c11941394a9b88a5c98095d5b6324e8572f63
juliguez <160885538+juliguez@users.noreply.github.com>
2024-02-27 23:24:32 +0000

    actualizado

--- SimuladorBancario/CuentaCorriente.py
+++ SimuladorBancario/CuentaCorriente.py
@@ -3,4 +3,7 @@ class CuentaCorriente:
     # Atributos
     saldo= 0
 
-    
+    def ConsignarSaldo (self):
+        nsaldo = self.saldo + 150000
+        self.saldo = nsaldo
+        return "El nuevo saldo mas el valor asignado es" + self.saldo
