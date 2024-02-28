b1fd0cf1422063724650759cc46fa2dd92af6bdc
juliguez <160885538+juliguez@users.noreply.github.com>
2024-02-28 15:48:32 +0000

    nuevo

--- Empleado/Empleado.py
+++ Empleado/Empleado.py
@@ -6,9 +6,11 @@ class Empleado:
     sexo = ""
     # 1= masculino 2= femenino
     salario = 0
+
     '''---------------------------------------------------
+    # Metodos
     ------------------------------------------------------'''
-    # Metodos 
+
     def CambiarSalario(self, nuevosalario):
     # Aqui va el codigo del metodo 
         return 0
@@ -40,3 +42,16 @@ class Empleado:
         nsalario = nsalario + self.salario
         self.salario = nsalario
         return "El nuevo salario es de:"+self.salario
+
+    def DuplicarSalario (self):
+        # Aqui va el codigo 
+        # Forma 1
+        self.salario = self.salario*2
+        # Forma 2
+        self.salario *= 2
+
+    def CalcularSalarioAnual (self):
+        # Aqui va el codigo 
+        asalario = self.salario * 12
+        return asalario
+
