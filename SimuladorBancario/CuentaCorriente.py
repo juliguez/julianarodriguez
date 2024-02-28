cf2ffd46a5b7091a873e5f45df99990bdd82ce83
juliguez <160885538+juliguez@users.noreply.github.com>
2024-02-28 01:49:46 +0000

    mejorar

--- SimuladorBancario/CuentaCorriente.py
+++ SimuladorBancario/CuentaCorriente.py
@@ -11,3 +11,9 @@ class CuentaCorriente:
         nsaldo = self.saldo + 150000
         self.saldo = nsaldo
         return " El nuevo saldo mas el valor asignado es " + self.saldo
+    
+    def RetirarValor (self):
+        nsaldo = self.saldo - 150000
+        self.saldo = nsaldo
+        return " El nuevo saldo menos el valor retirado es " + self.saldo
+    
